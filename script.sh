a891bfb43fcd22e11dd535af811b8eb38ef656d2
podlada1 <damian.podlaski24@gmail.com>
2019-02-03 13:14:29 +0000

    skrypt

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+zmienna=$(curl -I http://google.com 2>/dev/null | head -n 1 |awk '{print $2}')
+
+while true; do
+
+if [[ $zmienna = 200 ]]; then 
+echo OK
+else 
+echo ERROR
+
+fi
+
+sleep 5
+done
